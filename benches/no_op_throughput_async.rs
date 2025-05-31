af60d4d3a1f304c3918218896b9bc6a71fe7ab89
chillfish8 <hburt2003@gmail.com>
2025-05-31 12:14:33 +0100

    Add async benchmark

---
+++ benches/no_op_throughput_async.rs
@@ -0,0 +1,84 @@
+//! Tests the throughput of the scheduler logic and io_uring using noop operations.
+
+use std::io;
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+
+use tokio::sync::Barrier;
+
+const NUM_OPS_PER_WORKER: usize = 10_000;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let concurrency_levels = [1, 2, 4, 8, 16];
+
+    let configs = [
+        i2o2::builder(),
+        i2o2::builder().with_sqe_polling(true),
+        i2o2::builder()
+            .with_sqe_polling(true)
+            .with_sqe_polling_timeout(Duration::from_millis(100)),
+    ];
+
+    for config in configs {
+        eprintln!("running benchmark for config: {config:?}");
+        for (run_id, num_workers) in concurrency_levels.iter().enumerate() {
+            eprintln!("  {run_id} - run with {num_workers} concurrent tasks");
+            bench_with_config(config.clone(), *num_workers).await?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn bench_with_config(
+    builder: i2o2::I2o2Builder,
+    num_workers: usize,
+) -> io::Result<()> {
+    let (thread_handle, scheduler_handle) = builder.try_spawn::<()>()?;
+
+    let barrier = Arc::new(Barrier::new(num_workers + 1));
+
+    let mut worker_handles = Vec::with_capacity(num_workers);
+    for _ in 0..num_workers {
+        let barrier = barrier.clone();
+        let handle = scheduler_handle.clone();
+
+        let th = tokio::spawn(async move {
+            barrier.wait().await;
+
+            for _ in 0..NUM_OPS_PER_WORKER {
+                let op = i2o2::opcode::Nop::new().build();
+                let reply = unsafe { handle.submit_async(op, None).await.unwrap() };
+                let result = reply.await;
+                assert_eq!(result, Ok(0));
+            }
+
+            Ok::<_, io::Error>(0)
+        });
+
+        worker_handles.push(th);
+    }
+
+    barrier.wait().await;
+    eprintln!("    > run begin");
+
+    let start = Instant::now();
+    for worker in worker_handles {
+        worker.await.unwrap()?;
+    }
+    let elapsed = start.elapsed();
+    let total_ops = num_workers * NUM_OPS_PER_WORKER;
+    let ops_per_sec = total_ops as f32 / elapsed.as_secs_f32();
+    eprintln!(
+        "    > completed run, {elapsed:?}, {total_ops} op/total, {ops_per_sec:.2} op/sec"
+    );
+
+    drop(scheduler_handle);
+
+    thread_handle.join().expect("executor panicked")?;
+
+    Ok(())
+}
